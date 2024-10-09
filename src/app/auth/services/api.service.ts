cb06d09117a0d3e7b76a6f4b94c0c5b1418319d4
Fedecardozo <Fedecardozo.a@gmail.com>
2024-10-08 21:20:30 -0300

    Juego preguntados con estilos sin funcionamiento

---
+++ src/app/auth/services/api.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Pais } from '../../models/pais';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  url = 'https://restcountries.com/v3.1/all';
+  http = inject(HttpClient);
+
+  constructor() {}
+
+  getPaises() {
+    return this.http.get(this.url);
+  }
+}
--- src/app/pages/juegos/preguntados/preguntados.component.ts
+++ src/app/pages/juegos/preguntados/preguntados.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Pais } from '../../../models/pais';
+import { ApiService } from '../../../auth/services/api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-preguntados',
   standalone: true,
   imports: [],
   templateUrl: './preguntados.component.html',
-  styleUrl: './preguntados.component.css'
+  styleUrl: './preguntados.component.css',
 })
 export class PreguntadosComponent {
+  listaPaises: Pais[] = [];
+  private apiRest: ApiService = inject(ApiService);
+  pais: Pais | null = null;
+  sub?: Subscription;
 
+  ngOnInit(): void {
+    this.sub = this.apiRest.getPaises().subscribe((paises: any) => {
+      paises.map((auxPais: any) => {
+        let pais: Pais = new Pais(
+          auxPais.name.common,
+          auxPais.region,
+          auxPais.flags.svg
+        );
+
+        this.listaPaises.push(pais);
+      });
+
+      console.log(this.listaPaises);
+      this.cargarPais();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+
+  cargarPais() {
+    if (this.listaPaises.length) {
+      console.log('entro');
+      this.pais =
+        this.listaPaises[this.numeroRandom(this.listaPaises.length - 1)];
+      console.log(this.pais);
+    }
+  }
+
+  numeroRandom(max: number) {
+    return Math.floor(Math.random() * max);
+  }
 }
